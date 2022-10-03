8f1dcdbeb511bd5a68cdb5afbd7b8eaa0ae751ea
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-03 19:22:29 +0800

    add login page prototype

---
+++ client/src/components/Login.tsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { log } from 'console';
+import React, { useEffect, useState } from 'react';
+
+type LoginState = {
+    username: string;
+    password: string;
+};
+
+const Login = () => {
+    const [state, setState] = useState<LoginState>({
+        username: "",
+        password: ""
+    });
+
+    const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({
+            username: event.target.value,
+            password: state.password
+        });
+    }
+
+    const onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setState({
+            username: state.username,
+            password: event.target.value 
+        })
+    }
+
+    const onSubmit = (event: React.FormEvent<HTMLInputElement>) => {
+        axios.post('/api/login', state);
+    }
+
+    return (
+        <form>
+            <input type='text' name="username" value={state.username} onChange={onUsernameChange} /> <br />
+            <input type="password" name="password" value={state.password} onChange={onPasswordChange} /> <br />
+            <input type='submit' name="submit" onClick={onSubmit} />
+        </form>
+    );
+}
+
+export default Login;
