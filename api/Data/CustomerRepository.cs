d5086df684f0bf20fb2b082a419a04deb804d0cf
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-05 15:42:04 +0800

    add: generic repository

---
+++ api/Data/CustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using api.Models;
+using System.Runtime.CompilerServices;
+using System.Collections;
+
+namespace api.Data
+{
+    public class CustomerRepository : Repository<Customer>
+    {
+
+    }
+}
---
+++ api/Data/Repository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using api.Models;
+using System.Linq.Expressions;
+
+namespace api.Data
+{
+
+    public abstract class Repository<T> where T : class
+    {
+        protected readonly DbContext dbContext;
+        private DbSet<T>  dbSet;
+
+        public Repository(DbContext _context)
+        {
+            dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            dbSet = dbContext.Set<T>();
+        }
+        public async Task<T?> Get(int id)
+        {
+            return await dbSet.FindAsync(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return dbSet.ToList<T>();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return dbSet.Where(predicate);
+        }
+
+        public async void Create(T obj)
+        {
+            await dbSet.AddAsync(obj);
+        }
+
+        public async void Create(IEnumerable<T> objects)
+        {
+            await dbSet.AddRangeAsync(objects);
+        }
+
+        public void Remove(T obj)
+        {
+            dbSet.Remove(obj);
+        }
+        public void Remove(IEnumerable<T> obj)
+        {
+            dbSet.RemoveRange(obj);
+        }
+    }
+}
