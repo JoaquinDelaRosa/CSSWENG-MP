657c71fd7529aa15dcea8ae750a073d92d2cffbd
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-08 12:57:26 +0800

    add: SHA512 + UTF encryption algorithm

---
+++ api/Controllers/Crypto/CryptoStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Controllers.Crypto
+{
+    public abstract class CryptoStrategy
+    {
+        public abstract string Encrypt(string plainText);
+        public abstract bool IsEqual(string x, string y);
+    }
+}
---
+++ api/Controllers/Crypto/EncrypterManager.cs
@@ -0,0 +1,22 @@
+﻿
+namespace api.Controllers.Crypto
+{
+    public class EncrypterManager
+    {
+        private readonly CryptoStrategy strategy;
+        public EncrypterManager(CryptoStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public bool IsEqual(string x, string y)
+        {
+            return strategy.IsEqual(x, y);
+        }
+
+        public string Encrypt(string s)
+        {
+            return strategy.Encrypt(s);
+        }
+    }
+}
