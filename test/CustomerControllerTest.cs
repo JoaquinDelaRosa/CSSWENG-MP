b2e41ec1337f83b31dfcf08f58a3957169b595cc
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-12 14:52:22 +0800

    add: xunit project for unit testing

---
+++ test/CustomerControllerTest.cs
@@ -0,0 +1,11 @@
+namespace test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/Usings.cs
@@ -0,0 +1,7 @@
+global using Xunit;
+
+global using api.Controllers;
+global using api.Controllers.Crypto;
+
+global using api.Data;
+global using api.Models;
