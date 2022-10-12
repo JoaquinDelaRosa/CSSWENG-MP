eebb4f30c9b2b82d0f3a744d7d0ac57b47b64230
akn5 <104754302+alain-cheng@users.noreply.github.com>
2022-10-12 23:43:54 +0800

    refactor: moved imports to Usings.cs

--- test/CustomerControllerTest.cs
+++ test/CustomerControllerTest.cs
@@ -1,6 +1,3 @@
-using api.Views;
-using Microsoft.EntityFrameworkCore;
-
 namespace test
 {
     public class CustomerControllerTest
--- test/Usings.cs
+++ test/Usings.cs
@@ -3,6 +3,8 @@ global using Xunit;
 global using api.Controllers;
 global using api.Controllers.Crypto;
 
+global using Microsoft.EntityFrameworkCore;
+
 global using api.Data;
 global using api.Models;
 global using api.Views;
