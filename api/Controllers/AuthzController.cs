a36b2714440eb560428ac3eb9fd17c0ae93d270e
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-16 08:53:00 +0800

    fix: registration should not request middle name

--- api/Controllers/AuthzController.cs
+++ api/Controllers/AuthzController.cs
@@ -29,7 +29,6 @@ namespace api.Controllers
             public string Password { get; set; } = "";
 
             public string FirstName { get; set; } = "";
-            public string MiddleName { get; set; } = "";
             public string LastName { get; set; } = "";
         }
 
