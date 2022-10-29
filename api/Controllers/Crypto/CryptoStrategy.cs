af9b04c307f6b397e4fe0985946eb23d00f83295
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-29 19:17:23 +0800

    setup!! prep for refactoring from c# to TS

--- api/Controllers/Crypto/CryptoStrategy.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace api.Controllers.Crypto
-{
-    public abstract class CryptoStrategy
-    {
-        public abstract string Encrypt(string plainText);
-        public abstract bool IsEqual(string x, string y);
-    }
-}
