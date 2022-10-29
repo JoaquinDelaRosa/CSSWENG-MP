50787cc2cf0d31ff4f578c24720ca08503fc2d3e
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-29 19:21:32 +0800

    setup!! backend

---
+++ server/app.ts
@@ -0,0 +1,55 @@
+import * as express from 'express';
+import { AddressInfo } from "net";
+import * as path from 'path';
+
+import routes from './routes/index';
+import users from './routes/user';
+
+const debug = require('debug')('my express app');
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err[ 'status' ] = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+        res.status(err[ 'status' ] || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => { // eslint-disable-line @typescript-eslint/no-unused-vars
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function () {
+    debug(`Express server listening on port ${(server.address() as AddressInfo).port}`);
+});
