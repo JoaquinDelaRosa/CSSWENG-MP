af9e9796ecd070ba08b5c0bba63f5143ce07b984
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-10-31 15:58:14 +0800

    add: vehicle get controllers

--- api/routes/item-controllers/vehicle.ts
+++ api/routes/item-controllers/vehicle.ts
@@ -3,13 +3,21 @@
  */
 
 import express = require('express');
-import { Customer } from '../../models/customer';
+import { Vehicle } from '../../models/vehicle';
 import { API_PREFIX } from '../api';
 
 const router = express.Router();
 
-router.get(API_PREFIX + "customer/all", (req: express.Request, res: express.Response) => {
-    
+router.get(API_PREFIX + "Vehicles/all", async function (req: express.Request, res: express.Response) {
+    const result = await Vehicle.find({});
+    res.json(result);
 });
+
+router.get(API_PREFIX + "Vehicles/id", async function (req: express.Request, res: express.Response) {
+    const result = await Vehicle.find({id : req.query.id});
+    res.json(result);
+});
+
+
 
 export default router;
