3a74f11cddff1ebacb3e80e842c854c96d675d69
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-05 15:36:01 +0800

    fix: enum routes should be get

--- api/routes/enums.ts
+++ api/routes/enums.ts
@@ -2,8 +2,8 @@ import express = require('express');
 import controller from "../controllers/enums";
 const router = express.Router();
 
-router.post('/user/roles', controller.roles);
-router.post('/order/statuses', controller.statuses); 
-router.post('/order/types', controller.types); 
+router.get('/user/roles', controller.roles);
+router.get('/order/statuses', controller.statuses); 
+router.get('/order/types', controller.types); 
 
 export default router;
