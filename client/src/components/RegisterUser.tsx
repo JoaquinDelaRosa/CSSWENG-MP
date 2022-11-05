fa17c620e651d016718d678171473dc24e352377
tomatojojo <joshejercito01@gmail.com>
2022-11-05 15:36:59 +0800

    add: joaquin told me to push it

--- client/src/components/RegisterUser.tsx
+++ client/src/components/RegisterUser.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import '../style/RegistFull.css';
+import '../style/RegistDiv.css';
 import { createAPIEndpoint, ENDPOINTS } from '../api';
 
 interface RegistrationState {
@@ -35,39 +37,48 @@ const RegisterUser = () => {
     };
 
     return (
-        <div>
-            <p> Register </p>
-            <form>
-                <label>First Name</label>
-                <input type="text"
-                    name="firstName"
-                    onChange={(e) => { onInputChange("firstName", e.target.value); }} />
-                <br />
+        <div className="FullPage">
+            <div className="registerBox">
+                <div className="leftBG">
+                </div>
+                <div className="registLogo">
+                </div>
+                <div className="registerForm">
+                    <form className="registerUI">
+                        <label>First Name</label>
+                        <input type="text"
+                            name="firstName"
+                            onChange={(e) => { onInputChange("firstName", e.target.value); }} />
+                        <br />
 
-                <label>Last Name</label>
-                <input type="text"
-                    name="lastName"
-                    onChange={(e) => { onInputChange("lastName", e.target.value); }} />
-                <br />
+                        <label>Last Name</label>
+                        <input type="text"
+                            name="lastName"
+                            onChange={(e) => { onInputChange("lastName", e.target.value); }} />
+                        <br />
 
-                <label>Username</label>
-                <input type="text"
-                    name="username"
-                    onChange={(e) => { onInputChange("username", e.target.value); }} />
-                <br />
+                        <label>Username</label>
+                        <input type="text"
+                            name="username"
+                            onChange={(e) => { onInputChange("username", e.target.value); }} />
+                        <br />
 
-                <label>Password</label>
-                <input type="password"
-                    name="password"
-                    onChange={(e) => { onInputChange("password", e.target.value); }} />
-                <br />
+                        <label>Password</label>
+                        <input type="password"
+                            name="password"
+                            onChange={(e) => { onInputChange("password", e.target.value); }} />
+                        <br />
 
-                <input type='button'
-                    name="submit"
-                    onClick={onSubmit}
-                    value={"submit"} />
-            </form>
-        </div>  
+                        <input type='button'
+                            name="submit"
+                            onClick={onSubmit}
+                            value={"submit"} />
+                    </form>
+                    <p >Already have an account? <span className="redDialogue">Login here.</span></p>
+                </div>
+            </div>
+        </div>
+          
     );
 }
 
