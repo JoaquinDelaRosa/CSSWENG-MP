af3a29a95ed7dfa4ef29e23788b70ced5e0ad1ee
Sakui <ethan_saquilayan@dlsu.edu.ph>
2022-11-06 20:35:43 +0800

    update: all roles added to routes

--- api/routes/enums.ts
+++ api/routes/enums.ts
@@ -1,9 +1,13 @@
 import express = require('express');
 import controller from "../controllers/enums";
+import { ALL_ROLES, Roles } from '../models/enum';
+import validateRole from '../middleware/roleValidation';
+import validateToken from '../middleware/authValidation';
+
 const router = express.Router();
 
-router.get('/user/roles', controller.roles);
-router.get('/order/statuses', controller.statuses); 
-router.get('/order/types', controller.types); 
+router.get('/user/roles', validateToken, validateRole(ALL_ROLES), controller.roles);
+router.get('/order/statuses', validateToken, validateRole(ALL_ROLES), controller.statuses); 
+router.get('/order/types', validateToken, validateRole(ALL_ROLES), controller.types); 
 
 export default router;
--- api/routes/order.ts
+++ api/routes/order.ts
@@ -1,13 +1,16 @@
 import express = require('express');
 import controller from '../controllers/order';
+import { ALL_ROLES, Roles } from '../models/enum';
+import validateRole from '../middleware/roleValidation';
+import validateToken from '../middleware/authValidation';
 
 const router = express.Router();
 
-router.get('/all', controller.all);
-router.get('/id', controller.id);
-router.post('/create', controller.create);
-router.post('/update', controller.update);
-router.delete('/delete', controller.remove);
-router.get('/filter', controller.filter);
+router.get('/all', validateToken, validateRole(ALL_ROLES), controller.all);
+router.get('/id', validateToken, validateRole(ALL_ROLES), controller.id);
+router.post('/create', validateToken, validateRole([Roles.ADMIN, Roles.VIEW_EDIT]), controller.create);
+router.post('/update', validateToken, validateRole([Roles.ADMIN, Roles.VIEW_EDIT]), controller.update);
+router.delete('/delete', validateToken, validateRole([Roles.ADMIN, Roles.VIEW_EDIT]), controller.remove);
+router.get('/filter', validateToken, validateRole(ALL_ROLES), controller.filter);
 
 export default router;
