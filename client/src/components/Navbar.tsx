967188b61c2db47f14dd1704cc6b34694ddcb9ff
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-08 15:02:28 +0800

    add: navbar functionality

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,27 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../api/routes";
 import "../style/NavStyle.css"
 
-const Button = (props: {buttonName : string}) => {
+const Button = (props: {buttonName : string, to: string}) => {
+    const navigation = useNavigate();
     return (
-            <button className="navButton"> {props.buttonName} </button>
+        <button className="navButton" onClick={(e) => {e.preventDefault(); navigation(props.to)}}> {props.buttonName} </button>
     )
 }
 
 const Navbar = () => {
-
+    
     return (
-        
-            <header className="NavWrapper">
-                <nav className="Navbar">
-                    <img className="Logo" src={require("../style/images/TOPTECH AUTOWORKS LOGO.png")} alt="logo"></img>
-                    <div className="ButtonWrapper">
-                        <Button buttonName={"Orders"}/>
-                        <Button buttonName={"Customers"}/>
-                        <Button buttonName={"Vehicles"}/>
-                        <Button buttonName={"Users"}/>
-                    </div>
-                </nav>
-                <div className="RedBar"></div>
-            </header>
+        <header className="NavWrapper">
+            <nav className="Navbar">
+                <img className="Logo" src={require("../style/images/TOPTECH AUTOWORKS LOGO.png")} alt="logo"></img>
+                <div className={"ButtonWrapper"}>
+                    <Button buttonName={"Orders"} to={ROUTES.orders}/>
+                    <Button buttonName={"Customers"} to={ROUTES.customers}/>
+                    <Button buttonName={"Vehicles"} to={ROUTES.vehicles}/>
+                    <Button buttonName={"Users"} to={ROUTES.orders}/>
+                </div>
+            </nav>
+            <div className="RedBar"></div>
+        </header>
         
     );
 }
