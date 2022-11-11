b728881966c205a9f1b2f53ff9c923d1dc9c80bf
jared-limjoco <limjocojared9@gmail.com>
2022-11-12 03:44:48 +0800

    add: user table delete css

--- client/src/components/users/DeleteUser.tsx
+++ client/src/components/users/DeleteUser.tsx
@@ -13,8 +13,8 @@ export const DeleteUser = (props : {user : User, observer : Function}) => {
     }
 
     return (
-      <div>
-        <button onClick={onSubmit}> Delete </button>
+      <div className="deleteBtn">
+        <button onClick={onSubmit}><i className="deleteIcon"></i> Delete </button>
       </div> 
     );
 }
