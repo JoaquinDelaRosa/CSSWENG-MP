6d8197f5cafc695bc518d8748f8b0b57da24e6a0
Sakui <ethan_saquilayan@dlsu.edu.ph>
2022-11-13 00:26:06 +0800

    fix: CORS header error

--- client/src/utils/RefreshToken.tsx
+++ client/src/utils/RefreshToken.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL, createAPIEndpoint, ENDPOINTS, updateToken } from "../api";
 
 const refreshToken = () => {
-    axios.post(BASE_URL + 'api/' + ENDPOINTS.refreshToken, {}, { headers : {"Access-Control-Allow-Credentials" : true, 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*',} , withCredentials : true })
+    axios.post(BASE_URL + 'api/' + ENDPOINTS.refreshToken, {}, {withCredentials : true })
     .then((res) => {
         console.log(res)
     })
