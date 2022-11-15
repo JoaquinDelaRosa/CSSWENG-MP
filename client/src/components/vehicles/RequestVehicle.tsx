12bf6e939714c2b5bc9667b953d7e98f96383607
tomatojojo <joshejercito01@gmail.com>
2022-11-15 10:57:35 +0800

    update: edit modal styling

--- client/src/components/vehicles/RequestVehicle.tsx
+++ client/src/components/vehicles/RequestVehicle.tsx
@@ -14,30 +14,30 @@ export const RequestVehicle = (props : {setResponse : Function, default? : Vehic
 
     return (
         <div>
-            <p>Editing Vehicle Table:</p>
+            <p className="vehModalHead">Editing Vehicle Table:</p>
             <br />
             <form className="formStyle" onSubmit={onSubmit}>
                 <div className="licensePlateTag">
-                    <label htmlFor='licensePlate'>License Plate</label>
+                    <label htmlFor='licensePlate' className="vehicleSubText">License Plate</label>
                     <input {... register('licensePlate', {required: true, pattern: isLicensePlate})}
-                    type="text" name="licensePlate" defaultValue={props.default?.licensePlate}/>
+                    type="text" name="licensePlate" defaultValue={props.default?.licensePlate} className="vehicleSubField"/>
                     {errors.licensePlate && <p>License Plate is Required</p>}
                 </div>
                 <div className="manufactTag">
-                    <label htmlFor='manufacturer'>Manufacturer</label>
+                    <label htmlFor='manufacturer' className="vehicleSubText">Manufacturer</label>
                     <input {... register('manufacturer', {required: true, pattern: isAlphabetic})}
-                    type="text" name="manufacturer" defaultValue={props.default?.manufacturer}/>
+                    type="text" name="manufacturer" defaultValue={props.default?.manufacturer} className="vehicleSubField"/>
                     {errors.manufacturer && <p>Manufacturer is Required</p>}
                 </div>
                 <div className="modelTag">
-                    <label htmlFor='model'>Model</label>
+                    <label htmlFor='model' className="vehicleSubText">Model</label>
                     <input {... register('model', {required: true, pattern: isAlphaNumeric})}
-                    type="text" name="model" defaultValue={props.default?.model}/>
+                    type="text" name="model" defaultValue={props.default?.model} className="vehicleSubField"/>
                     {errors.model && <p>Model is Required</p>}
                 </div>
                 <div className="yearManufTag">
-                    <label htmlFor='yearManufactured'>Year Manufactured</label>
-                    <select  {...register('yearManufactured', {valueAsNumber: true, required: true})} 
+                    <label htmlFor='yearManufactured' className="vehicleSubText">Year Manufactured</label>
+                    <select className="vehicleSubField" {...register('yearManufactured', {valueAsNumber: true, required: true})} 
                         defaultValue= {(props.default && props.default.yearManufactured) ? 
                             props.default.yearManufactured.valueOf(): "DEFAULT"}>
                         <option key={0} value="DEFAULT" disabled>  -- Select Year -- </option>
@@ -53,7 +53,7 @@ export const RequestVehicle = (props : {setResponse : Function, default? : Vehic
                 </div>
                 <br />
                 <br />
-                <input type="button" name="submit" className="submit" onClick={onSubmit} value={"Submit"}/>
+                <input type="button" name="submit" className="submit" onClick={onSubmit} value={"SUBMIT"}/>
             </form>
         </div> 
     );
