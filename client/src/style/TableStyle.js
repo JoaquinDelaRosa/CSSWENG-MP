1542ebe9f120a99809884b000923a21d09cd412f
Sakui <104416692+ethansaqui@users.noreply.github.com>
2022-11-21 16:06:59 +0800

    add: table styling setup

---
+++ client/src/style/TableStyle.js
@@ -0,0 +1,15 @@
+import styles from 'styled-components';
+
+const TableWrapper = styles.div`
+    > table {
+        color: black;
+        width: 100%;
+        font-size: 16px;
+        border-collapse: collapse;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+    } 
+`
+
+export {TableWrapper}
