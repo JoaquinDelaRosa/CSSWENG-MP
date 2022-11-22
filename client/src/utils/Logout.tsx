efdbef6647fbb8b33e2b35aefe53016489f51941
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-22 21:50:54 +0800

    fix logout usenavigate should be in useeffect

--- client/src/utils/Logout.tsx
+++ client/src/utils/Logout.tsx
@@ -1,4 +1,5 @@
 
+import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { createAPIEndpoint } from "../api"
 import { ENDPOINTS } from "../api/endpoints"
@@ -7,12 +8,18 @@ import { removeRole } from "./CheckRole"
 
 export const Logout = (props: {setIsLoggedIn : Function}) => {
     const navigation = useNavigate()
+
+    useEffect(() => {
+        navigation(ROUTES.login);
+    }, [props.setIsLoggedIn])
+
     createAPIEndpoint(ENDPOINTS.logout).post({})
-    .then(response => {
-        props.setIsLoggedIn(false);
+    .then((response) => {
         sessionStorage.setItem("isLoggedIn", "false");
         removeRole();
-        navigation(ROUTES.login)
+    })
+    .then(() => {
+        props.setIsLoggedIn(false);
     })
     .catch((err) => {
         console.log(err)
