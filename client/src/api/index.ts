744e8489e582645c5827b1816cf4cb9e99f6c8e6
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-11-27 17:46:22 +0800

    deploy: update client path

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const Cookies = require('js-cookie')
 
-export const BASE_URL = 'http://localhost:3000/'
+export const BASE_URL = 'https://autoworks-logger-api.netlify.app/'
 
 
 export const createAPIEndpoint = (endpoint : string) => {
