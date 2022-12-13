a7f9e833df77367e3a3b8e7443af604ab6e92460
JoaquinDelaRosa <joaquin_delarosa@dlsu.edu.ph>
2022-12-13 15:35:48 +0800

    deploy: fix warnings

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const Cookies = require('js-cookie')
 
 const DEPLOYMENT_BASE_URL = 'https://autoworks-api.up.railway.app/'
-const DEVELOPMENT_BASE_URL = "http://localhost:3000/"
+// const DEVELOPMENT_BASE_URL = "http://localhost:3000/"
 
 export const BASE_URL = DEPLOYMENT_BASE_URL
 
